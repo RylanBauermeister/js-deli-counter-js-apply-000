514248d4fece30dca4ffb3efa3b6eb62a9bb6e4d
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-13 20:58:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+// var position = 1;
+
 function takeANumber(line, name){
-  line.push(name);
-  return "Welcome, " + name + ". You are number " + line.length + " in line."
+  // line.push(position++);
+  return "Welcome, " + name + ". You are number " + line.push(name) + " in line."
 }
 
 function nowServing(line){
   if(isEmpty(line)){
     return "There is nobody waiting to be served!"
   }
-  
   return "Currently serving " + line.shift() + ".";
 }
 
@@ -19,13 +20,17 @@ function currentLine(line){
   let result = "The line is currently: ";
   
   for(let i = 0; i < line.length; i++){
-    result += (i+1) + ". " + line[i] + ", "
+    result += `${i+1}. ${line[i] + (i === line.length-1 ? "" : ", ")}`;
   }
   
-  //Cut the last comma off and return.
-  return result.slice(0, result.length-2)
+  return result;
 }
 
 function isEmpty(line){
   return line.length === 0
 }
+
+// Array.prototype.push = function(item){
+//   this.splice(this.length,0, item);
+//   return this;
+// }
